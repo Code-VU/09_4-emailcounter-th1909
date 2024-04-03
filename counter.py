cfc76d523a15573356bfdad6083d2b389290a5b4
unknown <you@example.com>
2024-04-02 19:13:01 -0500

    9.4 complete

--- counter.py
+++ counter.py
@@ -3,7 +3,25 @@ def countEmail():
     name = input("Enter file:")
     if len(name) < 1 : name = "mbox-short.txt"
     handle = open(name)
+
+    senders: dict = {}
+
+    for line in handle:
+        if line.startswith('From '):
+            line = line.rstrip()
+            line = line.split()
+            sender: list = line[1]
+            senders[sender] = senders.get(sender, 0) + 1
+    
+    the_sender = None
+    largest = -1
+    for key, value in senders.items():
+        if value > largest:
+            largest = value
+            the_sender = key
         
+    print(the_sender, largest)
+    
 
 ## if you want to test locally run > python counter.py
 if __name__ == "__main__":
